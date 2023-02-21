0bb804ac8b081574184479cf98047163073542e4
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-02-21 16:44:20 +0000

    anchor updated in Apps.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about the React Spoodlebook app
         </a>
       </header>
     </div>
